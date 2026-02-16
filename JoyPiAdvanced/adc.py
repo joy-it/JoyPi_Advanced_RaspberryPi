6f801895d067f81b5e17c882a12b37fde23bc402
KeeckAOE <90447407+KeeckAOE@users.noreply.github.com>
2026-02-16 13:09:40 +0100

    Controll CS manually
    To prevent kernel to block GPIO8, CS is now manually selected

--- JoyPiAdvanced/adc.py
+++ JoyPiAdvanced/adc.py
@@ -1,4 +1,5 @@
 from spidev import SpiDev
+import RPi.GPIO as GPIO
     
 class adc:
     SYSTEM_STATUS = 0x00
@@ -8,7 +9,7 @@ class adc:
     PIN_CFG = 0x05
     SEQUENCE_CFG = 0x10
     
-    def __init__(self, bus = 0, device = 0):
+    def __init__(self, bus = 0, device = 0, cs = 8):
         """
         initialize ADC
         bus - select bus
@@ -18,6 +19,11 @@ class adc:
         self.spi = SpiDev()
         self.open()
         self.spi.max_speed_hz = 1000000 # 1MHz
+        self.spi.mode = 0
+        self.cs = cs
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.cs, GPIO.OUT, initial=GPIO.HIGH)
  
     def open(self):
         """
@@ -36,8 +42,12 @@ class adc:
         self.write_register(self.PIN_CFG, 0x00)
         self.write_register(self.SEQUENCE_CFG, 0x00)
         self.write_register(self.CHANNEL_SEL, channel)
+        GPIO.output(self.cs, GPIO.LOW)
         self.spi.readbytes(2)
+        GPIO.output(self.cs, GPIO.HIGH)
+        GPIO.output(self.cs, GPIO.LOW)
         data = self.spi.readbytes(2)
+        GPIO.output(self.cs, GPIO.HIGH)
         return ((data[0]<<8) | data[1]) >>4
  
     def read_voltage(self, channel, value = None): 
@@ -55,13 +65,16 @@ class adc:
         end SPI communication
         """
         self.spi.close()
+        GPIO.cleanup()
         
     def read_register(self, reg):
         """
         returns value from a specific register
         reg - register which should be read from
         """
+        GPIO.output(self.cs, GPIO.LOW)
         self.spi.writebytes([self.READ_CMD, reg, 0x00])
+        GPIO.output(self.cs, GPIO.HIGH)
         return self.spi.readbytes(1)
         
     def write_register(self, reg, data):
@@ -70,7 +83,9 @@ class adc:
         reg - register in which should be written
         data - value which should be written into the register
         """
-        self.spi.writebytes([self.WRITE_CMD, reg, data])  
+        GPIO.output(self.cs, GPIO.LOW)
+        self.spi.writebytes([self.WRITE_CMD, reg, data])
+        GPIO.output(self.cs, GPIO.HIGH)
         
     def read_status(self):
         """
@@ -81,4 +96,4 @@ class adc:
     
         
                  
-                                                                
+                                                                                                                              
