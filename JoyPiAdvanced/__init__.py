4dfa4d71722d8256c5c08a3fd2f3683b04c2cb20
KeeckAOE <90447407+KeeckAOE@users.noreply.github.com>
2026-02-18 12:27:43 +0100

    Set LEDMatrix as optional import

--- JoyPiAdvanced/__init__.py
+++ JoyPiAdvanced/__init__.py
@@ -1,10 +1,14 @@
 from .servomotor import servomotor
 from .stepmotor import stepmotor
-from .LEDMatrix import LEDMatrix
 from .gyroscope import gyroscope
 from .barometer import barometer
 from .buttonmatrix import buttonmatrix
 from .colour import colour
 from .adc import adc
+
+try:
+    from .LEDMatrix import LEDMatrix
+except Exception:
+    LEDMatrix = None
 
 name = "JoyPiAdvanced"
